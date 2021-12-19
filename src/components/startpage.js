45d9e3d418b54b4938b94ed0136e324454848937
Khan-Sajid <sajidkhanjnv0@gmail.com>
2021-12-19 23:04:49 +0530

    second commit

--- src/components/startpage.js
+++ src/components/startpage.js
@@ -6,18 +6,18 @@ function StartPage({ fetchData }) {
     const [country, setCountry] = useState("");
 
     function checkDisabled() {
-        if (country === "") {
+        if (!country.length) {
             setDisabled(true);
         } else {
             setDisabled(false);
         }
     }
     useEffect(() => {
-
+        checkDisabled();
     }, [country])
     return (
         <div className="input-div">
-            <input type="text" placeholder="country name" value={country} onChange={(event) => { checkDisabled(); setCountry(event.target.value) }}></input>
+            <input type="text" placeholder="country name" onChange={(event) => { setCountry(event.target.value); }}></input>
             {disabled
                 ? <button className="submit-button" type="submit" value="Submit" disabled={disabled}>Enter</button>
                 : <button className="submit-button" type="submit" value="Submit" onClick={() => { setCountry(""); fetchData(country) }}>Enter</button>}
